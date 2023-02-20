d0670fac0d1769f6530a22103752c0c4b9748d65
caoayu <1401262639@qq.com>
2023-02-21 00:34:08 +0800

    修改banner，及一些配置

--- src/main/java/top/caoayu/livehelper/controller/HomeController.java
+++ src/main/java/top/caoayu/livehelper/controller/HomeController.java
@@ -1,23 +1,18 @@
 package top.caoayu.livehelper.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
 @SpringBootApplication
 public class HomeController {
     @RequestMapping("/")
-    public ResponseEntity<Map<String, Object>> home(){
-        Map<String, Object> response = new HashMap<>();
-        response.put("data","Hello Live Helper");
-        response.put("message","OK");
-        response.put("code",200);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> home(){
+        return new ResponseEntity<>("hello world", HttpStatus.OK);
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,1 @@
-create schema if not exists livehelper collate utf8mb4_unicode_ci;
+create database if not exists livehelper collate utf8mb4_unicode_ci;
